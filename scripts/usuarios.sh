e6112c48db7324cd502e7d635496eed33a249aba
Brenda <tu-correo@ejemplo.com>
2026-05-12 00:39:02 +0000

    fix: agregando contenido al script de usuarios

---
+++ scripts/usuarios.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Automatización de creación de usuarios para Soluciones Tecnológicas
+echo "Creando usuario Brenda-Admin..."
+sudo useradd -m Brenda-Admin
+echo "Usuario creado con éxito."
